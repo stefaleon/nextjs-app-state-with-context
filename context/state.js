0e70cc495ade6a55a59d3f70e163404bba4ad381
unknown <stefaleon@gmail.com>
2021-05-16 01:59:52 +0300

    how to update the context state

--- context/state.js
+++ context/state.js
@@ -1,16 +1,36 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 const AppContext = createContext();
 
 export function AppWrapper({ children }) {
   let sharedState = {
-    aString: 'initial string',
-    numbersArray: [1, 2, 3],
+    // aString: 'initial string',
+    // numbersArray: [1, 2, 3],
     anObject: { a: 'initial value' },
+  };
+  const [aString, setAString] = useState('initial string');
+  const [numbersArray, setNumbersArray] = useState([1, 2, 3]);
+
+  const updateTheString = (newValue) => {
+    setAString(newValue);
+  };
+
+  const updateTheFirstNumber = () => {
+    setNumbersArray([9, 2, 3]);
   };
 
   return (
-    <AppContext.Provider value={sharedState}>{children}</AppContext.Provider>
+    <AppContext.Provider
+      value={{
+        ...sharedState,
+        aString,
+        numbersArray,
+        updateTheString,
+        updateTheFirstNumber,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
   );
 }
 
--- pages/test.js
+++ pages/test.js
@@ -6,6 +6,14 @@ import AppContext from '../context/state';
 export default function test() {
   const ctx = useContext(AppContext);
 
+  const changeTheString = () => {
+    ctx.updateTheString('NEW VALUE!');
+  };
+
+  const changeTheFirstNumber = () => {
+    ctx.updateTheFirstNumber();
+  };
+
   return (
     <div className={styles.container}>
       <p>test</p>
@@ -13,6 +21,8 @@ export default function test() {
       <p>The string is: {ctx.aString}</p>
       <p>The first number is: {ctx.numbersArray[0]}</p>
       <p>The value of a is: {ctx.anObject.a}</p>
+      <button onClick={changeTheString}>Change the string</button>
+      <button onClick={changeTheFirstNumber}>Change the first number</button>
     </div>
   );
 }
